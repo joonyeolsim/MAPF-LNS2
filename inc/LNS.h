3fb01cfcded5c20d4afb74350af9d7c88a40aef0
simjoonyeol <jysim@u.sogang.ac.kr>
2023-11-25 18:12:14 +0900

    Reformat code

--- inc/LNS.h
+++ inc/LNS.h
@@ -2,13 +2,13 @@
 #include "BasicLNS.h"
 #include "InitLNS.h"
 
-//pibt related
-#include "simplegrid.h"
-#include "pibt_agent.h"
-#include "problem.h"
+// pibt related
 #include "mapf.h"
 #include "pibt.h"
+#include "pibt_agent.h"
 #include "pps.h"
+#include "problem.h"
+#include "simplegrid.h"
 #include "winpibt.h"
 
 enum destroy_heuristic { RANDOMAGENTS, RANDOMWALK, INTERSECTION, DESTORY_COUNT };
@@ -17,66 +17,60 @@ enum destroy_heuristic { RANDOMAGENTS, RANDOMWALK, INTERSECTION, DESTORY_COUNT }
 // increase it if no progress is made for a while
 // decrease it if replanning fails to find any solutions for several times
 
-class LNS : public BasicLNS
-{
-public:
-    vector<Agent> agents;
-    double preprocessing_time = 0;
-    double initial_solution_runtime = 0;
-    int initial_sum_of_costs = -1;
-    int sum_of_costs_lowerbound = -1;
-    int sum_of_distances = -1;
-    int restart_times = 0;
+class LNS : public BasicLNS {
+ public:
+  vector<Agent> agents;
+  double preprocessing_time = 0;
+  double initial_solution_runtime = 0;
+  int initial_sum_of_costs = -1;
+  int sum_of_costs_lowerbound = -1;
+  int sum_of_distances = -1;
+  int restart_times = 0;
 
-    LNS(const Instance& instance, double time_limit,
-        const string & init_algo_name, const string & replan_algo_name, const string & destory_name,
-        int neighbor_size, int num_of_iterations, bool init_lns, const string & init_destory_name, bool use_sipp,
-        int screen, PIBTPPS_option pipp_option);
-    ~LNS()
-    {
-        delete init_lns;
-    }
-    bool getInitialSolution();
-    bool run();
-    void validateSolution() const;
-    void writeIterStatsToFile(const string & file_name) const;
-    void writeResultToFile(const string & file_name) const;
-    void writePathsToFile(const string & file_name) const;
-    string getSolverName() const override { return "LNS(" + init_algo_name + ";" + replan_algo_name + ")"; }
-private:
-    InitLNS* init_lns = nullptr;
-    string init_algo_name;
-    string replan_algo_name;
-    bool use_init_lns; // use LNS to find initial solutions
-    destroy_heuristic destroy_strategy = RANDOMWALK;
-    int num_of_iterations;
-    string init_destory_name;
-    PIBTPPS_option pipp_option;
+  LNS(const Instance& instance, double time_limit, const string& init_algo_name, const string& replan_algo_name,
+      const string& destory_name, int neighbor_size, int num_of_iterations, bool init_lns,
+      const string& init_destory_name, bool use_sipp, int screen, PIBTPPS_option pipp_option);
+  ~LNS() { delete init_lns; }
+  bool getInitialSolution();
+  bool run();
+  void validateSolution() const;
+  void writeIterStatsToFile(const string& file_name) const;
+  void writeResultToFile(const string& file_name) const;
+  void writePathsToFile(const string& file_name) const;
+  string getSolverName() const override { return "LNS(" + init_algo_name + ";" + replan_algo_name + ")"; }
 
+ private:
+  InitLNS* init_lns = nullptr;
+  string init_algo_name;
+  string replan_algo_name;
+  bool use_init_lns;  // use LNS to find initial solutions
+  destroy_heuristic destroy_strategy = RANDOMWALK;
+  int num_of_iterations;
+  string init_destory_name;
+  PIBTPPS_option pipp_option;
 
-    PathTable path_table; // 1. stores the paths of all agents in a time-space table;
-    // 2. avoid making copies of this variable as much as possible.
-    unordered_set<int> tabu_list; // used by randomwalk strategy
-    list<int> intersections;
+  PathTable path_table;  // 1. stores the paths of all agents in a time-space table;
+  // 2. avoid making copies of this variable as much as possible.
+  unordered_set<int> tabu_list;  // used by randomwalk strategy
+  list<int> intersections;
 
-    bool runEECBS();
-    bool runCBS();
-    bool runPP();
-    bool runPIBT();
-    bool runPPS();
-    bool runWinPIBT();
+  bool runEECBS();
+  bool runCBS();
+  bool runPP();
+  bool runPIBT();
+  bool runPPS();
+  bool runWinPIBT();
 
+  MAPF preparePIBTProblem(vector<int>& shuffled_agents);
+  void updatePIBTResult(const PIBT_Agents& A, vector<int>& shuffled_agents);
 
-    MAPF preparePIBTProblem(vector<int>& shuffled_agents);
-    void updatePIBTResult(const PIBT_Agents& A, vector<int>& shuffled_agents);
+  void chooseDestroyHeuristicbyALNS();
 
-    void chooseDestroyHeuristicbyALNS();
+  bool generateNeighborByRandomWalk();
+  bool generateNeighborByIntersection();
 
-    bool generateNeighborByRandomWalk();
-    bool generateNeighborByIntersection();
-
-    int findMostDelayedAgent();
-    int findRandomAgent() const;
-    void randomWalk(int agent_id, int start_location, int start_timestep,
-                    set<int>& neighbor, int neighbor_size, int upperbound);
+  int findMostDelayedAgent();
+  int findRandomAgent() const;
+  void randomWalk(int agent_id, int start_location, int start_timestep, set<int>& neighbor, int neighbor_size,
+                  int upperbound);
 };
--- inc/common.h
+++ inc/common.h
@@ -1,43 +1,43 @@
 #pragma once
-#include <tuple>
-#include <list>
-#include <vector>
-#include <set>
-#include <map>
+#include <boost/heap/pairing_heap.hpp>
+#include <boost/unordered_map.hpp>
+#include <boost/unordered_set.hpp>
+#include <chrono>
 #include <ctime>
 #include <fstream>
-#include <iostream>     // std::cout, std::fixed
-#include <iomanip>      // std::setprecision
-#include <chrono>
+#include <iomanip>   // std::setprecision
+#include <iostream>  // std::cout, std::fixed
+#include <list>
+#include <map>
+#include <set>
+#include <tuple>
 #include <utility>
-#include <boost/heap/pairing_heap.hpp>
-#include <boost/unordered_set.hpp>
-#include <boost/unordered_map.hpp>
+#include <vector>
 
-using boost::heap::pairing_heap;
-using boost::heap::compare;
 using boost::unordered_map;
 using boost::unordered_set;
-using std::vector;
-using std::list;
-using std::set;
-using std::map;
-using std::get;
-using std::tuple;
-using std::make_tuple;
-using std::pair;
-using std::make_pair;
-using std::tie;
-using std::min;
-using std::max;
-using std::shared_ptr;
-using std::make_shared;
+using boost::heap::compare;
+using boost::heap::pairing_heap;
+using std::cerr;
 using std::clock;
 using std::cout;
 using std::endl;
+using std::get;
+using std::list;
+using std::make_pair;
+using std::make_shared;
+using std::make_tuple;
+using std::map;
+using std::max;
+using std::min;
 using std::ofstream;
-using std::cerr;
+using std::pair;
+using std::set;
+using std::shared_ptr;
 using std::string;
+using std::tie;
+using std::tuple;
+using std::vector;
 using namespace std::chrono;
 typedef std::chrono::high_resolution_clock Time;
 typedef std::chrono::duration<float> fsec;
@@ -46,37 +46,37 @@ typedef std::chrono::duration<float> fsec;
 #define MAX_COST INT_MAX / 2
 #define MAX_NODES INT_MAX / 2
 
-struct PathEntry
-{
-	int location = -1;
-	explicit PathEntry(int loc = -1) { location = loc; }
+struct PathEntry {
+  int location = -1;
+  explicit PathEntry(int loc = -1) { location = loc; }
 };
 
 typedef vector<PathEntry> Path;
 std::ostream& operator<<(std::ostream& os, const Path& path);
 bool isSamePath(const Path& p1, const Path& p2);
 
-struct IterationStats
-{
-    int sum_of_costs;
-    double runtime;
-    int num_of_agents;
-    string algorithm;
-    int sum_of_costs_lowerbound;
-    int num_of_colliding_pairs;
-    IterationStats(int num_of_agents, int sum_of_costs, double runtime, const string& algorithm,
-                   int sum_of_costs_lowerbound = 0, int num_of_colliding_pairs = 0) :
-            num_of_agents(num_of_agents), sum_of_costs(sum_of_costs), runtime(runtime),
-            sum_of_costs_lowerbound(sum_of_costs_lowerbound), algorithm(algorithm),
-            num_of_colliding_pairs(num_of_colliding_pairs) {}
+struct IterationStats {
+  int sum_of_costs;
+  double runtime;
+  int num_of_agents;
+  string algorithm;
+  int sum_of_costs_lowerbound;
+  int num_of_colliding_pairs;
+  IterationStats(int num_of_agents, int sum_of_costs, double runtime, const string& algorithm,
+                 int sum_of_costs_lowerbound = 0, int num_of_colliding_pairs = 0)
+      : num_of_agents(num_of_agents),
+        sum_of_costs(sum_of_costs),
+        runtime(runtime),
+        sum_of_costs_lowerbound(sum_of_costs_lowerbound),
+        algorithm(algorithm),
+        num_of_colliding_pairs(num_of_colliding_pairs) {}
 };
 
-struct PIBTPPS_option{
-    int windowSize ;
-    bool winPIBTSoft ;
-    int timestepLimit ;
+struct PIBTPPS_option {
+  int windowSize;
+  bool winPIBTSoft;
+  int timestepLimit;
 };
-
 
 // Only for three-tuples of std::hash-able types for simplicity.
 // You can of course template this struct to allow other hash functions
@@ -91,4 +91,3 @@ struct PIBTPPS_option{
         return h1 ^ h2 ^ h3;
     }
 };*/
-
